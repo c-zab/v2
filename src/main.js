5a2ecbb0df5f2113c4e57c5f1a7e9e0c605dd18d
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-03-20 22:27:20 -0400

    Adding MDBootstrap & FontAwesome libraries

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 
 require("./assets/css/styles.css");
+require("../node_modules/mdbootstrap/css/bootstrap.min.css");
+require("../node_modules/mdbootstrap/css/mdb.min.css");
 
 Vue.config.productionTip = false;
 
