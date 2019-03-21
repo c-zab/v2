b2d8d6ca18abeb8193ca12899d485aba35eab702
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-03-21 15:59:09 -0400

    Adding Raleway & Montserrat fonts

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="cover" class="cover">
+  <div id="cover">
     <div class="cover-image">
       <div class="jumbotron centered shadow-none bg-transparent">
         <div class="container">
-          <h1 class="font-weight-bold text-white display-4">Hello, I'm Carlos Zabaleta.</h1>
-          <h1 class="font-weight-bold text-success">I'm a full-stack web developer.</h1>
+          <h1 class="text-white display-4">Hello, I'm Carlos Zabaleta.</h1>
+          <h1 class="text-success">I'm a full-stack web developer.</h1>
           <div class="button" dest="about">
             View my work
             <i class="fas fa-arrow-right fa-rotate-90"></i>
@@ -31,7 +31,15 @@ export default {};
 #cover .jumbotron {
   width: 100%;
 }
+h1 {
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+}
 
+p {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+}
 .button {
   padding: 10px 20px;
   color: white;
