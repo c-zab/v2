14c8c5668b5d26404e3745c0da6bea99890c9492
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-03-27 23:57:06 -0400

    Styling the titles and subtitles

--- src/HeadCover.vue
+++ src/HeadCover.vue
@@ -3,6 +3,7 @@
     <Cover/>
     <Navbar/>
     <div class="container">
+      <h1 class="title pt-5 pb-2 text-center">About</h1>
       <p>
         <i class="fas fa-pencil-ruler"></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
         Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
@@ -21,6 +22,7 @@
         Omnis eum vero id fugit totam ea hic alias est. Asperiores ipsum adipisci est rerum. Quae dignissimos dolores sed. Et accusamus eos earum architecto.
         Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
       </p>
+      <h1 class="title pt-5 pb-2 text-center">Skills</h1>
       <p>
         <i class="fas fa-pencil-ruler"></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
         Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
@@ -39,12 +41,7 @@
         Omnis eum vero id fugit totam ea hic alias est. Asperiores ipsum adipisci est rerum. Quae dignissimos dolores sed. Et accusamus eos earum architecto.
         Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
       </p>
-      <p>
-        <i class="fas fa-pencil-ruler"></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
-        Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
-        Omnis eum vero id fugit totam ea hic alias est. Asperiores ipsum adipisci est rerum. Quae dignissimos dolores sed. Et accusamus eos earum architecto.
-        Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
-      </p>
+      <h1 class="title pt-5 pb-2 text-center">Portfolio</h1>
       <p>
         <i class="fas fa-pencil-ruler"></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
         Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
@@ -58,9 +55,6 @@
         Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
       </p>
     </div>
-    <Cover/>
-    <Cover/>
-    <Cover/>
   </div>
 </template>
 
@@ -78,4 +72,14 @@ export default {
 </script>
 
 <style>
+.title {
+  font-weight: 700;
+}
+
+.header-bar {
+  margin: 0px 0px 50px 0px;
+  width: 70px;
+  height: 5px;
+  background: #444649;
+}
 </style>
