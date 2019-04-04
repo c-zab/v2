a4888bd2971dd33771cbf7324039ae7087687c83
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-04-04 14:55:44 -0400

    Hover interaction in the icons

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="about-sec" class="container">
     <Title title="ABOUT"/>
-    <div class="container">
+    <div class="container pb-5">
       <div class="row text-center align-self-center">
         <div class="col-md">
           <img class="me-picture" src="../assets/images/me.jpg" align="center" alt="image">
@@ -20,10 +20,8 @@
           <Language name="PHP" percentage="70"/>
           <Language name="C#" percentage="60"/>
           <Language name="Typescript" percentage="65"/>
-          <Language name="Java" percentage="65"/>
           <Language name="VueJS" percentage="70"/>
           <Language name="React" percentage="55"/>
-          <Language name="Git" percentage="80"/>
           <Language name="JS" percentage="70"/>
           <Language name="CSS" percentage="80"/>
           <Language name="HTML" percentage="75"/>
@@ -32,29 +30,54 @@
     </div>
     <div class="container pt-4">
       <div class="row text-center">
-        <div class="col-md-6 col-lg bg-danger py-3">
-          <i class="fas fa-tachometer-alt fa-5x"></i>
-          <h3>Fast</h3>
+        <div class="col-md-6 col-lg py-3">
+          <i
+            class="about-icon fas fa-5x"
+            @mouseover="hoverBolt = true"
+            @mouseleave="hoverBolt = false"
+            :class="{'amber-text':hoverBolt, 'fa-bolt':hoverBolt,'fa-tachometer-alt':!hoverBolt}"
+          ></i>
+          <h3 @mouseover="hoverBolt = true" @mouseleave="hoverBolt = false">Fast</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
         </div>
-        <div class="col-md-6 col-lg bg-info py-3">
-          <i class="fas fa-mobile-alt fa-5x"></i>
-          <h3>Responsive</h3>
+        <div class="col-md-6 col-lg py-3">
+          <i
+            class="about-icon fas fa-5x"
+            @mouseover="hoverMobile = true"
+            @mouseleave="hoverMobile = false"
+            :class="{'indigo-text':hoverMobile, 'fa-laptop':!hoverMobile,'fa-mobile-alt':hoverMobile}"
+          ></i>
+          <h3 @mouseover="hoverMobile = true" @mouseleave="hoverMobile = false">Responsive</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
         </div>
-        <div class="col-md col-lg bg-danger py-3">
-          <i class="far fa-lightbulb fa-5x"></i>
-          <h3>Intuitive</h3>
+        <div class="col-md col-lg py-3">
+          <i
+            class="about-icon far fa-5x"
+            @mouseover="hoverLight=true"
+            @mouseleave="hoverLight=false"
+            :class="{'fa-grin-beam': hoverLight, 'fa-lightbulb': !hoverLight, 'teal-text': hoverLight}"
+          ></i>
+          <h3 @mouseover="hoverLight=true" @mouseleave="hoverLight=false">Intuitive</h3>
+          <p>yellow rgba-yellow-strong Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
+        </div>
+        <div class="col-md col-lg py-3">
+          <i
+            class="about-icon fa-5x"
+            @mouseover="hoverRocket = true"
+            @mouseleave="hoverRocket = false"
+            :class="{'fas fa-rocket':hoverRocket,'far fa-paper-plane':!hoverRocket, 'red-text':hoverRocket}"
+          ></i>
+          <h3 @mouseover="hoverRocket = true" @mouseleave="hoverRocket = false">Dinamic</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
         </div>
-        <div class="col-md col-lg bg-info py-3">
-          <i class="fas fa-rocket fa-5x"></i>
-          <h3>Dinamic</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
-        </div>
-        <div class="col-md-12 col-lg-12 bg-success py-3">
-          <i class="fas fa-laptop-code fa-5x"></i>
-          <h3>Code</h3>
+        <div class="col-md-12 col-lg-12 py-3">
+          <i
+            class="about-icon fa-5x"
+            @mouseover="hoverCode = true"
+            @mouseleave="hoverCode = false"
+            :class="{'fas fa-laptop-code':hoverCode, 'far fa-file-code':!hoverCode, 'light-blue-text': hoverCode }"
+          ></i>
+          <h3 @mouseover="hoverCode = true" @mouseleave="hoverCode = false">Code</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tempore fuga numquam</p>
         </div>
       </div>
@@ -71,11 +94,24 @@ export default {
   components: {
     Title,
     Language
+  },
+  data() {
+    return {
+      hoverBolt: false,
+      hoverMobile: false,
+      hoverLight: false,
+      hoverRocket: false,
+      hoverCode: false
+    };
   }
 };
 </script>
 
 <style scoped>
+.about-icon {
+  width: 100%;
+}
+
 img.me-picture {
   width: 70%;
   clip-path: polygon(
