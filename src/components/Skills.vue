26cbaa6ec2583f2cf503eae41d47aee4de1e8411
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-04-10 18:24:02 -0400

    Adding skills' table

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,31 +1,25 @@
 <template>
   <div id="skills-sec" class="container">
     <Title title="SKILLS"/>
-    <p>
-      <i class="fas fa-pencil-ruler"></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
-      Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
-      Omnis eum vero id fugit totam ea hic alias est. Asperiores ipsum adipisci est rerum. Quae dignissimos dolores sed. Et accusamus eos earum architecto.
-      Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
-      <i
-        class="fas fa-pencil-ruler"
-      ></i>Ut et explicabo odio magnam harum error voluptas qui alias. Quaerat expedita ea natus cupiditate impedit aliquid rerum officiis eum. Impedit nemo enim placeat. Voluptatum quasi et voluptas exercitationem sed aut earum molestias. Sunt sint nihil quaerat labore voluptas. Commodi iste aut impedit rerum.
-      Sit magni officiis sunt optio quibusdam accusamus dicta voluptatum. Dolores et et molestiae doloremque dolorem odit dolorum id sed. Ipsum enim molestiae vel nemo consequatur a deleniti dolorum. Est incidunt ea qui voluptatem adipisci velit esse ducimus harum.
-      Omnis eum vero id fugit totam ea hic alias est. Asperiores ipsum adipisci est rerum. Quae dignissimos dolores sed. Et accusamus eos earum architecto.
-      Autem consequatur ex iusto sint voluptas tenetur voluptate velit. Eos et nostrum soluta odit. Optio asperiores veniam ipsa vel ex neque dolorum quia. Quo debitis non et qui.
-      <i
-        class="fas fa-pencil-ruler"
-      ></i>
-    </p>
+    <div class="container">
+      <div class="row">
+        <SkillsTable title="Backend"/>
+        <SkillsTable title="Frontend"/>
+        <SkillsTable title="Others"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Title from "./includes/Title";
+import SkillsTable from "./includes/SkillsTable";
 
 export default {
   name: "skills-sec",
   components: {
-    Title
+    Title,
+    SkillsTable
   }
 };
 </script>
---
+++ src/components/includes/SkillsTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-md">
+    <h2>{{ title }}</h2>
+    <div class="list-group-flush">
+      <div class="list-group-item py-2">
+        <div class="row">
+          <div class="col-3">
+            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
+          </div>
+          <div class="col">
+            <h4 class="pt-2">Photos</h4>
+          </div>
+        </div>
+      </div>
+      <div class="list-group-item py-2">
+        <div class="row">
+          <div class="col-3">
+            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
+          </div>
+          <div class="col">
+            <h4 class="pt-2">Photos</h4>
+          </div>
+        </div>
+      </div>
+      <div class="list-group-item py-2">
+        <div class="row">
+          <div class="col-3">
+            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
+          </div>
+          <div class="col">
+            <h4 class="pt-2">Photos</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+.devicon-image {
+  font-size: 3rem;
+}
+</style>
