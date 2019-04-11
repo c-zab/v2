51c268617119f9855f98ba619e9cbe89c385e338
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-04-11 01:48:55 -0400

    Adding elements to skill's tables

--- src/components/includes/SkillsTable.vue
+++ src/components/includes/SkillsTable.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="col-md">
-    <h2>{{ title }}</h2>
+    <h3 class="py-4">{{ title }}</h3>
     <div class="list-group-flush">
-      <div class="list-group-item py-2">
-        <div class="row">
-          <div class="col-3">
-            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
-          </div>
-          <div class="col">
-            <h4 class="pt-2">Photos</h4>
-          </div>
-        </div>
-      </div>
-      <div class="list-group-item py-2">
-        <div class="row">
-          <div class="col-3">
-            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
-          </div>
-          <div class="col">
-            <h4 class="pt-2">Photos</h4>
-          </div>
-        </div>
-      </div>
-      <div class="list-group-item py-2">
-        <div class="row">
-          <div class="col-3">
-            <i class="devicon-image devicon-laravel-plain colored p-2"></i>
-          </div>
-          <div class="col">
-            <h4 class="pt-2">Photos</h4>
+      <div class="col-md">
+        <div class="list-group-flush">
+          <div v-for="item in table" v-bind:key="item.id" class="list-group-item py-2">
+            <div class="row">
+              <div class="col-3">
+                <div v-if="item.name=== 'Python'">
+                  <img src="https://icongr.am/devicon/python-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'Vue'">
+                  <img src="https://icongr.am/devicon/vuejs-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'Java'">
+                  <img src="https://icongr.am/devicon/java-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== '.NET'">
+                  <img src="https://icongr.am/devicon/dot-net-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'C#'">
+                  <img src="https://icongr.am/devicon/csharp-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'GIMP'">
+                  <img src="https://icongr.am/devicon/gimp-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'Linux'">
+                  <img src="https://icongr.am/devicon/linux-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'CSS3'">
+                  <img src="https://icongr.am/devicon/css3-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'NPM'">
+                  <img src="https://icongr.am/devicon/npm-original-wordmark.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'Slack'">
+                  <img src="https://icongr.am/devicon/slack-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'HTML'">
+                  <img src="https://icongr.am/devicon/html5-original.svg?size=45">
+                </div>
+                <div v-else-if="item.name=== 'Chrome Dev Tools'">
+                  <img src="https://icongr.am/devicon/chrome-original.svg?size=45">
+                </div>
+                <div v-else>
+                  <i :class="[item.classItem]"></i>
+                </div>
+              </div>
+              <div class="col">
+                <h4 class="pt-2">{{ item.name }}</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -38,14 +60,16 @@
 
 <script>
 export default {
-  props: {
-    title: String
-  }
+  props: ["title", "table"]
 };
 </script>
 
 <style scoped>
 .devicon-image {
   font-size: 3rem;
+}
+
+.devicon-vuejs-plain {
+  color: #3aa374;
 }
 </style>
