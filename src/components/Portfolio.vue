6ce0add28707b0f7af610c04cf7444e035ffa19d
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-04-22 15:57:29 -0500

    Adding cartItems in Mix

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -27,227 +27,29 @@
       </div>
     </div>
     <div class="containerMix row no-gutters">
-      <div class="mix backend col-md-4">
-        <div
-          class="card-image"
-          :style="{ backgroundImage: 'url(' + require('@/assets/images/cards.jpg') + ')' }"
-        >
-          <div
-            class="text-white text-center align-items-center rgba-black-strong py-5 px-4"
-            style="height:300px;"
-            @mouseover="show = true"
-            @mouseleave="show = false"
-          >
-            <transition name="bounce">
-              <div v-if="show">
-                <h5 class="pink-text">Backend</h5>
-                <h3 class="card-title pt-2">
-                  <strong>QueryFox</strong>
-                </h3>
-                <a class="btn btn-pink">
-                  <i class="fas fa-clone left"></i> Details
-                </a>
-              </div>
-            </transition>
-          </div>
-        </div>
-      </div>
-      <div class="mix frontend col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Frontend
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Czabalet</strong>
-              </h3>
-
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix backend col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Backend
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Anthony's Games</strong>
-              </h3>
-
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix frontend col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Frontend
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Delisa</strong>
-              </h3>
-
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix frontend col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Frontend
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Arrow Game</strong>
-              </h3>
-
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="mix backend col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Backend
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Books Library</strong>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
-                Odit sed qui, dolorum!.
-              </p>
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix leadership col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Leadership
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>AIESEC</strong>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
-                Odit sed qui, dolorum!.
-              </p>
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix leadership col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Leadership
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>TEDx</strong>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
-                Odit sed qui, dolorum!.
-              </p>
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mix leadership col-md-4">
-        <div
-          class="card card-image"
-          style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);"
-        >
-          <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-            <div>
-              <h5 class="pink-text">
-                <i class="fas fa-chart-pie"></i> Leadership
-              </h5>
-              <h3 class="card-title pt-2">
-                <strong>Google Development Group</strong>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
-                Odit sed qui, dolorum!.
-              </p>
-              <a class="btn btn-pink">
-                <i class="fas fa-clone left"></i> Details
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>-->
+      <cardItem :cardItem="queryfox"/>
+      <cardItem :cardItem="czabalet"/>
+      <cardItem :cardItem="games"/>
+      <cardItem :cardItem="delisa"/>
+      <cardItem :cardItem="space"/>
+      <cardItem :cardItem="books"/>
+      <cardItem :cardItem="aiesec"/>
+      <cardItem :cardItem="tedx"/>
+      <cardItem :cardItem="google"/>
     </div>
   </div>
 </template>
 
 <script>
 import Title from "./includes/Title";
+import cardItem from "./includes/cardItem";
 import mixitup from "mixitup";
 
 export default {
   name: "portfolio-sec",
   components: {
-    Title
+    Title,
+    cardItem
   },
   mounted() {
     var containerEl = document.querySelector(".containerMix");
@@ -255,34 +57,56 @@ export default {
   },
   data() {
     return {
-      show: false
+      show: false,
+      queryfox: {
+        title: "QueryFox",
+        image: "queryfox.jpg",
+        type: "Backend"
+      },
+      czabalet: {
+        title: "Czabalet",
+        image: "czabalet.jpg",
+        type: "Frontend"
+      },
+      games: {
+        title: "Games and Stuff",
+        image: "games.jpg",
+        type: "Backend"
+      },
+      delisa: {
+        title: "Delisa",
+        image: "delisa.jpg",
+        type: "Frontend"
+      },
+      space: {
+        title: "Space Shooter",
+        image: "space.jpg",
+        type: "Frontend"
+      },
+      books: {
+        title: "Books Library",
+        image: "books.jpg",
+        type: "Frontend"
+      },
+      aiesec: {
+        title: "AIESEC",
+        image: "aiesec.jpg",
+        type: "Leadership"
+      },
+      tedx: {
+        title: "TEDx",
+        image: "tedx.jpg",
+        type: "Leadership"
+      },
+      google: {
+        title: "Google Development Group",
+        image: "google.jpg",
+        type: "Leadership"
+      }
     };
   }
 };
 </script>
 
 <style scoped>
-.card-image {
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.bounce-enter-active {
-  animation: bounce-in 0.5s;
-}
-.bounce-leave-active {
-  animation: bounce-in 0.5s reverse;
-}
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.3);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
 </style>
---
+++ src/components/includes/cardItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="mix col-md-4" :class="mixVal">
+    <div
+      class="card-image"
+      :style="{ backgroundImage: 'url(' + require('@/assets/images/cards/queryfox.jpg') + ')' }"
+    >
+      <div
+        class="text-white text-center align-items-center rgba-black-strong py-5 px-4"
+        style="height:300px;"
+        @mouseover="show = true"
+        @mouseleave="show = false"
+      >
+        <transition name="bounce">
+          <div v-if="show">
+            <h5 class="pink-text">{{ cardItem.type }} - {{mixVal}}</h5>
+            <h3 class="card-title pt-2">
+              <strong>{{ cardItem.title }}</strong>
+            </h3>
+            <a class="btn btn-pink">
+              <i class="fas fa-clone left"></i> Details
+            </a>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { cardItem: Object },
+  data() {
+    return {
+      show: false,
+      mixVal: this.cardItem.type.toLowerCase()
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.3s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
