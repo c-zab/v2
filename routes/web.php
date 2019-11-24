da0af53b4249f63c9b630da738a8d20cc4ff3362
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-11-23 23:53:35 -0500

    Laravel mix Bug [assets]

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::get('/', 'PagesController@home');
+// Route::get('/', 'PagesController@home');
 
-// Route::get('{path}', function () {
-//   return view('home');
-// })->where('path', '(.*)');
+Route::get('{path}', function () {
+  return view('home');
+})->where('path', '(.*)');
 
 Route::post('/messages', 'MessagesController@store');
