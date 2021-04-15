67141c4abd68a552f236b2eefbd79fa48ce3afbc
Carlos Zabaleta <zabaletacopa@gmail.com>
2021-04-14 23:53:25 -0400

    Removing default frontend

--- src/main.js
+++ src/main.js
@@ -4,12 +4,7 @@ import './js/plugins';
 import './js/components';
 
 Vue.config.productionTip = false;
-/*
- * new Vue({
- *   render: h => h(App),
- * }).$mount('#app');
- */
 
 new Vue({
   el: '#app',
-});
+}).$mount('#app');
