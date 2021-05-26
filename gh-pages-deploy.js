b20af74c0f7d379eac11beeb0ffccde9f0e13581
Carlos Zabaleta <zabaletacopa@gmail.com>
2021-05-26 16:32:10 -0400

    Adding script for GitHub pages

---
+++ gh-pages-deploy.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+const execa = require('execa');
+const fs = require('fs');
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'deploy']);
+    // eslint-disable-next-line no-console
+    console.log('Building started...');
+    await execa('yarn', ['run', 'build']);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    await execa('git', ['--work-tree', folderName, 'add', '--all']);
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'deploy']);
+    console.log('Pushing to deploy...');
+    await execa('git', ['push', 'origin', 'HEAD:deploy', '--force']);
+    await execa('rm', ['-r', folderName]);
+    await execa('git', ['checkout', '-f', 'master']);
+    await execa('git', ['branch', '-D', 'deploy']);
+    console.log('Successfully deployed, check your settings');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
